049a9b02add1cf7d8cbbb1b598d06961f6d0785a
jgkimapex <jgkimapex@gmail.com>
2026-03-09 21:21:29 +0900

    Fix hydration error in layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <body className={`${inter.variable} ${outfit.variable} antialiased font-inter`}>
         <NextIntlClientProvider messages={messages}>
           {children}
